8e97c99351e89c7933aa20ae36139376d7916391
levonal <lwshabee@gmail.com>
2023-11-29 14:02:47 -0600

    added requirements

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('secrets.key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False).lower() == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split('')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
 AUTHENTICATION_BACKENDS = [
 
